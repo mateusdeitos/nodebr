8c1a3aaae6e12cc0d1f58355d63ec8f087d2f749
mateusdeitos <mate.deitos@hotmail.com>
2021-02-23 08:02:19 -0300

    testes

--- 8_testes-com-mocha/test.js
+++ 8_testes-com-mocha/test.js
@@ -2,16 +2,46 @@ const assert = require('assert');
 const nock = require('nock')
 const { obterPessoas } = require('../services/swapi');
 
-describe('Star Wars Tests', async () => {
+describe('Star Wars Tests', () => {
+
+    beforeAll(() => {
+        const response = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: [
+              {
+                name: 'R2-D2',
+                height: '96',
+                mass: '32',
+                hair_color: 'n/a',
+                skin_color: 'white, blue',
+                eye_color: 'red',
+                birth_year: '33BBY',
+                gender: 'n/a',
+                homeworld: 'http://swapi.dev/api/planets/8/',
+                films: [Array],
+                species: [Array],
+                vehicles: [],
+                starships: [],
+                created: '2014-12-10T15:11:50.376000Z',
+                edited: '2014-12-20T21:17:50.311000Z',
+                url: 'http://swapi.dev/api/people/3/'
+              }
+            ]
+          }
+          nock('https://swapi.dev/api/people').get('/?search=r2-d2&format=json').reply(200, response);
+    })
+
     it('deve buscar o r2d2', async () => {
-        const expected = [{ nome: 'R2-D2', peso: '96' }];
+        const expected = { nome: 'R2-D2', peso: '966' };
         const nomeBase = `r2-d2`;
 
-        obterPessoas(nomeBase).then(({ results }) => {
-            console.log(results);
-            const mapped = results.map(pessoa => ({ nome: pessoa.name, altura: pessoa.height }));
-            console.log(mapped)
-            assert.deepStrictEqual(mapped[0], expected);
-        });
+        const resultado = await obterPessoas(nomeBase)
+        
+        console.log(resultado);
+        const mapped = results.map(pessoa => ({ nome: pessoa.name, altura: pessoa.height }));
+        console.log(mapped)
+        assert.deepStrictEqual(mapped[0], expected);
     })
 })
--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const childProcess = require('child_process');
-const path = require('path');
-const fs = require('fs');
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = __importDefault(require("child_process"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 // Get all folders within project and map it with the prefix as index
 let folders = {};
-fs.readdirSync(__dirname, { withFileTypes: true })
+fs_1.default.readdirSync(__dirname, { withFileTypes: true })
     .filter(reg => reg.isDirectory())
     .forEach(dir => {
-        const idx = dir.name.split("_")[0];
-        folders[idx] = dir.name;
-    });
-
+    const idx = dir.name.split("_")[0];
+    folders[idx] = dir.name;
+});
 // Get index passed as argument, e.g: yarn start 1 -> index = 1
 const folderIndex = process.argv[2];
-
 const folder = folders[folderIndex];
 if (!folder) {
-    throw new Error('modulo nao encontrado', folderIndex);
+    throw new Error('modulo nao encontrado');
 }
-
 // Build path to file;
-const file = path.resolve(__dirname, folder, 'index.js');
-
-if (!fs.existsSync(file)) {
-    throw new Error('arquivo não existe', file);
+const file = path_1.default.resolve(__dirname, folder, 'index.js');
+if (!fs_1.default.existsSync(file)) {
+    throw new Error('arquivo não existe');
 }
-
 // Execute file script
 const params = process.argv.slice(2);
-childProcess.fork(file, params);
+child_process_1.default.fork(file, params);
+//# sourceMappingURL=index.js.map
